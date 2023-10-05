08be60eb1f8b217bcfedc428b5f5bf4357f15526
zubair <zubair.khan3@outlook.com>
2023-10-05 15:11:10 +0530

    signup form working, signup form templated desgin done

--- crm/members/forms.py
+++ crm/members/forms.py
@@ -5,7 +5,7 @@ from django import forms
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_name  = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email      = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email      = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     
 
 
@@ -13,10 +13,17 @@ class SignUpForm(UserCreationForm):
         model  = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
         widgets = {
-            'username' : forms.TextInput(attrs={'class':'form-control'}),
+           # 'username' : forms.TextInput(attrs={'class':'form-control'}),
             #'first_name' : forms.TextInput(attrs={'class':'form-control'}),
             #'last_name' : forms.TextInput(attrs={'class':'form-control'}),
             #'email' : forms.EmailField(attrs={'class':'form-control'}),
             #'password1' : forms.PasswordInput(attrs={'class':'form-control'}),
             #'password2' : forms.PasswordInput(attrs={'class':'form-control'}),
         } 
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm,self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
