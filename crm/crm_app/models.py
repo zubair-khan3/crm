3ec568ab7ed99ce40593d869bff5b5c608e75725
zubair <zubair.khan3@outlook.com>
2023-10-05 16:21:02 +0530

    created model, name Record, in crm_app,

--- crm/crm_app/models.py
+++ crm/crm_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+    created_on  = models.DateTimeField(auto_now_add=True)
+    first_name  = models.CharField(max_length=50)
+    last_name   = models.CharField(max_length=50)
+    email       = models.EmailField(max_length=50)
+    phone       = models.CharField(max_length=50)
+    city        = models.CharField(max_length=50)
+    address     = models.CharField(max_length=50)
+    zip_code    = models.CharField(max_length=50)
+     
+    def __str__(self):
+        return(f"{self.first_name} {self.last_name}")
